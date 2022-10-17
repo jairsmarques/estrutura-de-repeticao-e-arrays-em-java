8de1a4fbc2dd04a653c1fb9aab2744263bae120d
Jair Sousa <jairsmarques1126@gmail.com>
2022-10-17 11:03:05 -0300

    Adicionando o exercício 4

---
+++ src/br/com/dio/exercicios/loops/Ex4_ParEImpar.java
@@ -0,0 +1,36 @@
+package br.com.dio.exercicios.loops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que peça N números inteiros,
+calcule e mostre a quuantidade de números pares
+e a quantidade de números impares.
+ */
+public class Ex4_ParEImpar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int numero;
+        int quantPares = 0;
+        int quantImpares = 0;
+
+        System.out.println("Quantidade de números: ");
+        quantNumeros = scan.nextInt();
+
+        int count = 0;
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if (numero % 2 == 0) quantPares++;
+            else quantImpares++;
+
+            count++;
+        }while (count < quantNumeros);
+
+        System.out.println("Quantidade par: " + quantPares);
+        System.out.println("Quantidade impar: " + quantImpares);
+    }
+}
