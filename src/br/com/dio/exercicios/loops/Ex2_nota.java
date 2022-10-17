97025ca47a7e991ea069c99f652a317d7a03207b
Jair Sousa <jairsmarques1126@gmail.com>
2022-10-17 10:25:27 -0300

    Adicionando 0 exercício 2

--- src/br/com/dio/exercicios/loops/Ex2_nota.java
+++ src/br/com/dio/exercicios/loops/Ex2_nota.java
@@ -7,6 +7,20 @@ e continue pedindo
 até que o usuário informe um valor válido.
 */
 
-public class Ex2_nota {
+import java.util.Scanner;
 
+public class Ex2_nota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while (nota < 0 | nota > 10) {
+            System.out.println("Nota inválida! Digite novamente");
+            nota = scan.nextInt();
+        }
+    }
 }
